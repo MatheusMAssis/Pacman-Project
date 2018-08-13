2a3fda754a5cfdfb52ab27eb70ee31ad9098bb76
Matheus Assis <30660987+MatheusMAssis@users.noreply.github.com>
2018-08-13 00:15:37 -0300

    Create Map.py

---
+++ Map.py
@@ -0,0 +1,22 @@
+class Map():
+    
+    def __init__(self, x, y):
+        self.wall = False
+        self.small_dot = False
+        self.big_dot = False
+        self.eaten = False
+        self.x = x
+        self.y = y
+        
+    def draw_dot(self):
+        if self.small_dot:
+            if not self.eaten:
+                fill(255, 255, 0)
+                noStroke()
+                ellipse(self.x, self.y, 3, 3)
+                
+        elif self.big_dot:
+            if not self.eaten:
+                fill(255, 255, 0)
+                noStroke()
+                ellipse(self.x, self.y, 6, 6)
