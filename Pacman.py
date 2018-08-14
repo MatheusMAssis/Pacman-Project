7dbc91be7c08491f277d924a70a19bca3476bf4b
Matheus Assis <30660987+MatheusMAssis@users.noreply.github.com>
2018-08-14 19:45:21 -0300

    changing to PVector

--- Pacman.py
+++ Pacman.py
@@ -1,38 +1,53 @@
 class Pacman:
     
     def __init__(self):
-        self.x = 13*16 + 8 #initial position
-        self.y = 23*16 + 8
-        self.fact = 16 #"walk" factor
+        self.pos = PVector(13*16 + 8, 23*16 + 8)
+        self.vel = PVector(-1, 0)
+        self.dir = PVector(-1, 0)
+        self.turn = False
         self.score = 0
-        self.ddx = 0 #get track of the direction
-        self.ddy = 0 
-        self.move = False
   
     def draw_p(self):
         noStroke()
         fill(255, 235, 5)
-        ellipse(self.x, self.y, 20, 20)
+        ellipse(self.pos.x, self.pos.y, 20, 20)
     
-    def direction(self, dx, dy):
-        delay(30)
-        
-        self.move = True
-        if self.move:
-            self.x += dx * self.fact
-            self.y += dy * self.fact
-        self.ddx = dx
-        self.ddy = dy
-        
+    def move(self, table):
+        if self.turn:
+            self.pos.add(self.vel)
+    
     def check_position(self, table):
-        pos_x = (self.x - 8) / 16
-        pos_y = (self.y - 8) / 16
-        
-        if table[pos_y][pos_x].wall:
-            self.x -= self.ddx * self.fact
-            self.y -= self.ddy * self.fact
-        
-        if ((self.x - 8) % 16 == 0 and (self.y - 8) % 16 == 0):
-            if not table[pos_y][pos_x].eaten:
-                table[pos_y][pos_x].eaten = True
+        if ((self.pos.x - 8) % 16 == 0 and (self.pos.y - 8) % 16 == 0):
+            m_pos = PVector((self.pos.x - 8) / 16, (self.pos.y - 8) / 16)
+            if not table[floor(m_pos.y)][floor(m_pos.x)].eaten:
+                table[floor(m_pos.y)][floor(m_pos.x)].eaten = True
                 self.score += 1
+                
+                #if big_dot
+                
+            if table[floor(m_pos.y + self.dir.y)][floor(m_pos.x + self.dir.x)].wall:
+                if table[floor(m_pos.y + self.vel.y)][floor(m_pos.x + self.vel.x)].wall:
+                    self.turn = False
+                    return False
+                else:
+                    self.turn = True
+                    return True
+            else:
+                self.vel = PVector(self.dir.x, self.dir.y)
+                self.turn = True
+                return True
+        else:
+            if (self.pos.x + 10 * self.vel.x - 8) % 16 == 0 and (self.pos.y + 10 * self.vel.y - 8) % 16 == 0:
+                m_pos = PVector((self.pos.x + 10 * self.vel.x - 8) / 16, (self.pos.y + 10 * self.vel.y - 8) / 16)
+                if not table[floor(m_pos.y)][floor(m_pos.x)].eaten:
+                    table[floor(m_pos.y)][floor(m_pos.x)].eaten = True
+                    self.score += 1
+                    
+                    #if big_dot
+                    
+            if self.dir.x + self.vel.x == 0 and self.dir.y + self.vel.y == 0:
+                self.vel = PVector(self.dir.x, self.dir.y)
+                self.turn = True
+                return True
+            self.turn = True
+            return True
